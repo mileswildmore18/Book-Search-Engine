b765a1f8a5482c5a329c04be4bc730d57868106f
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-05 18:39:42 -0400

    Added the JWT token to the App jsx file along with the necessary files to work with the App jsx.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,42 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+const httpLink = createHttpLink({ //connecting to the graphql
+  url: '/graphql',
+})
+//JWT token
+const authLink = setContext((_, { headers }) => {
+  //saving in local storage
+  const token = localStorage.getItem('id_token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
+    <ApolloProvider client = {client}>
       <Navbar />
       <Outlet />
+    </ApolloProvider>
     </>
   );
 }
