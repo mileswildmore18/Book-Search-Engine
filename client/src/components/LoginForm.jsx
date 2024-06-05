ee3def52281113fbba32ac5322a9f039fdcefb62
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-05 19:47:19 -0400

    Added the login details for a user already signed up to login to the website.

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -30,15 +30,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } =await loginUser({
+        variables: {...userFormData}//user form
+      })
+      console.log(data)
+      Auth.login(data.loginUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
