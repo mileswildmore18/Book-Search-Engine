a38ebe2d0d6db6ecf44dbfc3e667619609ed448a
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-05 16:55:12 -0400

    Added the schemas folder with the index, resolver and typeDefs js files contained in the server folder. Also added the data for typeDefs.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+//Making a database for the types of book
+const typeDefs = `
+type User {
+    _id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String!
+    link: String
+    image: String
+}
+{
+    type Auth {
+        token: ID!
+        user: User
+    }
+{
+    type Query {
+        me: User
+    }
+{
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+`;
+
+module.exports = typeDefs;
